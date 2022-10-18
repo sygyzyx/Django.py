e9e46be354baadbdadc88ca589ff5c48730a968b
Aadesh <aadeshtw@gmail.com>
2022-10-18 19:59:31 +0545

    password reset and change works

--- MRBA/app/models.py
+++ MRBA/app/models.py
@@ -1,6 +1,12 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 
+class Password_Reset_Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    password_reset_token = models.CharField(max_length=100)
+    def __str__(self):
+        return self.user.email
 
 class Room_Name(models.Model):
     room_Name = models.CharField(max_length=122)
--- MRBA/app/urls.py
+++ MRBA/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.Index, name='index'),
@@ -14,5 +15,18 @@ urlpatterns = [
     path('Grantmeet', views.grantMeetView, name='grantmeet'),
     path('CancelBooking/<int:id>', views.cancelBookingView, name='cancelbook'),
     path('EditRoom/<int:id>', views.editRoomView, name='editRoom'),
-    path('DeleteRoom/<int:id>', views.deleteRoomBookingView, name='deleteRoom')
+    path('DeleteRoom/<int:id>', views.deleteRoomBookingView, name='deleteRoom'),
+    path('activate/<uidb64>/<token>', views.Activate, name='activate'),
+
+    #######################################
+    path('password_reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('password_change',auth_views.PasswordChangeView.as_view(),name="password_change"),
+    path('password_change_done',auth_views.PasswordChangeDoneView.as_view(),name="password_change_done"),
+
+
+
+    
 ]
